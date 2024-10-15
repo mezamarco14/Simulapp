b9c0cbd67d5d1453c8dbe34b17e0ea4093bd7fb4
24or341 <jc2021069822@virtual.upt.pe>
2024-10-15 11:00:32 -0500

    Avance de Login y Register 2

--- simulapp/lib/main.dart
+++ simulapp/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'src/data/conection.dart'; // Importa la función de inicialización de Firebase
+import 'package:firebase_auth/firebase_auth.dart';
+import 'src/data/conection.dart'; // Inicializa Firebase
+import 'src/iu/login.dart'; // Pantalla de Login
+import 'src/iu/register.dart'; // Pantalla de Registro
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,14 +15,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Firebase App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Firebase Connection'),
-        ),
-        body: Center(
-          child: Text('Firebase initialized successfully!'),
-        ),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: AuthCheck(), // Verifica el estado de autenticación
+    );
+  }
+}
+
+class AuthCheck extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // Si el usuario está autenticado, lo redirigimos a la pantalla principal
+        if (snapshot.connectionState == ConnectionState.active) {
+          User? user = snapshot.data;
+          if (user == null) {
+            return LoginScreen(); // Si no hay usuario, muestra la pantalla de login
+          } else {
+            return RegisterScreen(); // Si hay usuario, muestra la pantalla de registro o la principal
+          }
+        } else {
+          // Si el estado de conexión está cargando
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
     );
   }
 }
